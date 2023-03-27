c3dcd13229df0c7b87ec7cfc6180e645247a9247
Mohammed Haaris Syed <haaris.syed@hotmail.com>
2023-03-27 14:50:23 +0100

    add filtered searchbar with autocomplete

--- website/src/components/Searchbar.js
+++ website/src/components/Searchbar.js
@@ -2,22 +2,37 @@ import React, { useEffect, useState } from "react";
 import "../assets/css/Searchbar.css";
 
 function Searchbar(props) {
-  const [animeTitles, setAnimeTitles] = useState([])
+  const [animeTitles, setAnimeTitles] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
   const getAnimeTitles = async () => {
-    const titles = await fetch('/get_anime_titles').then(
-      resp => resp.json()
-    )
+    const titles = await fetch("/get_anime_titles").then((resp) => resp.json());
 
-    setAnimeTitles(titles)
-  }
+    setAnimeTitles(titles);
+  };
 
   useEffect(() => {
-    getAnimeTitles()
-  }, [])
-  
-  console.log(animeTitles)
+    getAnimeTitles();
+  }, []);
 
+  const handleFilter = (e) => {
+    const searchTerm = e.target.value;
+    const filteredTitles = animeTitles.filter((title) => {
+      return title.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    if (searchTerm === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(filteredTitles);
+      
+    }
+    props.setSearch(searchTerm);
+  };
+
+  const autoComplete = (search) => {
+    props.setSearch(search)
+  }
+   
   return (
     <div>
       <form className="search-box" onSubmit={props.handleSearch}>
@@ -26,13 +41,19 @@ function Searchbar(props) {
           placeholder="Enter an anime..."
           required
           value={props.search}
-          onChange={(e) => props.setSearch(e.target.value)}
+          onChange={handleFilter}
         />
-        <div className="search-results">
-          {animeTitles.slice(0,15).map((title, index) => {
-            return <p className="result-item">{title}</p>
-          })}
-        </div>
+        {filteredData.length !== 0 && (
+          <div className="search-results">
+            {filteredData.slice(0, 15).map((title, index) => {
+              return <p className="result-item" 
+              onClick={() => autoComplete(title)}
+              key={index}>
+                {title}
+                </p>;
+            })}
+          </div>
+        )}
       </form>
     </div>
   );
