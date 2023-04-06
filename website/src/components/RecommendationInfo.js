eba3de07d2b3634087d5cee2d594d60f1709e789
Mohammed Haaris Syed <haaris.syed@hotmail.com>
2023-04-06 13:37:36 +0100

    update recommendation information page

--- website/src/components/RecommendationInfo.js
+++ website/src/components/RecommendationInfo.js
@@ -10,24 +10,26 @@ function RecommendationInfo() {
           Upon clicking the filters icon next to the searchbar on the homepage,
           sliders with different values will appear on screen. These values are
           the weights used within the recommendation algorithm and can be
-          altered to your liking. Each slider represents a similarity type that
+          altered to your liking. Each slider represents a similarity type,
+          where the similarity type is indicated by the name of that slider, and
           is used to determine the similarity between anime.
           <h3>Content-based filtering:</h3>
           <p>
             Anime features, such as their genre, popularity, and average rating,
             are used to determine the similarity between different animes. Here,
             you are able to adjust the values of each similarity type. Changing
-            these values updates how much of each similarity type will be used
-            in the overall recommendation algorithm.
+            these values will reflect how much of each similarity type will be
+            used in the overall recommendation algorithm, with higher values
+            taking precedence over others.
           </p>
           <h3>Collaborative filtering:</h3>
           <p>
             Here, ratings given to animes by other users is the main factor for
             determining the similarity between anime. The ratings are separated
             into sub categories where each slider represents a different
-            similarity type. Changing these values updates how much each
+            rating category. Changing these values will reflect how much of each
             similarity type will be used in the overall recommendation
-            algorithm.
+            algorithm, with higher values taking precedence over others.
           </p>
         </h4>
         <h4>
